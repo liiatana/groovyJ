d54abfaa752698d1475b94668d5094f15ea4c239
liiatana <mail.ru>
2020-06-01 23:05:58 +0300

    delete FTPfolder -ver with script parameters

--- src/deleteFTPfolder.groovy
+++ src/deleteFTPfolder.groovy
@@ -1,15 +1,19 @@
-//Jenkins property=ActiveChoiceParameter/SingleSelect
-// Получение списка настроек окружения с FTP сервера ( ENVIROMENT)
-groovy.grape.Grape.grab(group:'commons-net', module:'commons-net', version:'3.6')
-
+// Удалить конфигурацию с FTP сервера
+//Jenkins :
+// 1. установить и настроить плагин https://plugins.jenkins.io/groovy/
+// 2. "положить" этот groovy-файл на машину Jenkins, например, /var/lib/jenkins/.groovy/deleteFTPfolder.groovy
+// 3. в разделе "Сборка" добавить шаг "Execute Groovy Script" и указать путь до скрипта(из. шага 2)
+@Grab(group='commons-net', module='commons-net', version='3.6')
 
 import org.apache.commons.net.ftp.FTPClient
 import org.apache.commons.net.ftp.FTPReply
 
+
 def server="172.29.17.219"
 def user="Jenkins"
 def pass="Jenkins1"
-def folderName="qwrty weyrui" // ENVIROMENT - имя параметра джоба, в котором хранятся названия папок
+
+def folderName=args[0] // ENVIROMENT - имя параметра джоба, в котором хранятся названия папок, передать как аргумент командной строки
 
 def ftpClient = new FTPClient()
 ftpClient.connect(server)
